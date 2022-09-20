b5d20dc49a3897b6d09509ce6318a2bda7dc7a46
mruggeroni <matiasruggeroni@gmail.com>
2022-09-20 01:45:26 -0300

    Adapting repository for deployment

--- api/index.js
+++ api/index.js
@@ -19,11 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const PORT = process.env.PORT;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {  // force: false para no resetear DB
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
   // try {
   //   await GET_ALL_GAMES();
