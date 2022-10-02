fea367b94cbd5668f75064b069f6d9aa4db136cf
mruggeroni <matiasruggeroni@gmail.com>
2022-10-02 20:39:21 -0300

    Setting all styles on SearchBar.jsx

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import s from "./SearchBar.module.css";
 
 
 export default function SearchBar({ handleSubmitSearch, handleInputSearchChange, handleLeaveSearch, name }) {
     return (
         <div>
             <form 
+                className={`${s.form}`}
                 name="search"
                 onSubmit={e => handleSubmitSearch(e)} 
             >
                 <input
+                    className={`${s.items} ${s.search}`}
                     type="text"
                     title="Search game"
                     placeholder="Search game"
@@ -17,6 +20,7 @@ export default function SearchBar({ handleSubmitSearch, handleInputSearchChange,
                 />
                 {
                     name && <button 
+                    className={`${s.items} ${s.buttonDelete}`}
                         type="button"
                         onClick={e => handleLeaveSearch(e)}
                     >
@@ -24,6 +28,7 @@ export default function SearchBar({ handleSubmitSearch, handleInputSearchChange,
                     </button>
                 }
                 <input 
+                    className={`${s.items} ${s.buttonSearch}`}
                     type="submit" 
                     value="Search" 
                 />
