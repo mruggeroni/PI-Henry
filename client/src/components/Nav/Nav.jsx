921093ce54fb101fbc24766e77edbe938d63f083
mruggeroni <matiasruggeroni@gmail.com>
2022-10-02 20:43:42 -0300

    styled Nav.jsx (featured position: fixed)

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -8,11 +8,23 @@ export default function Nav() {
     const icon = "https://res.cloudinary.com/dkf1okbsr/image/upload/v1663212306/PI/page_icon_ocrogz.jpg";
     
     return (
-        <div className={s.nav} >
-            <img className={s.icon_nav} src={icon} alt="icon" />
-            <NavLink className={s.link_nav} to="/home">Home</NavLink>
-            <NavLink className={s.link_nav} to="/home/about">About</NavLink>
-            <NavLink className={s.link_nav} to="/home/creategame">Create Game</NavLink>
-        </div>
+        <>
+            <div className={`${s.nav} ${s.nav_fixed}`} >
+                <NavLink className={`${s.link_nav} ${s.div_nav}`} to="/home">
+                    <img 
+                        className={s.icon_nav} 
+                        src={icon} 
+                        alt="icon" 
+                    />
+                    <h1 className={s.title_nav} >VideoGame App</h1>
+                </NavLink>
+                <div className={s.div_nav} >
+                    <NavLink className={s.link_nav} to="/home">Home</NavLink>
+                    <NavLink className={s.link_nav} to="/home/about">About</NavLink>
+                    <NavLink className={s.link_nav} to="/home/creategame">Create Game</NavLink>
+                </div>
+            </div>
+            <div className={s.nav} ></div>
+        </>
     );
 };
